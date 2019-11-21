bfb9ba3113ddffd312cee435c663634d3d51df71
Kirk Hilse <khilsedev@gmail.com>
2019-11-21 11:24:02 -0800

    Added angle offset so characters appear under selection mark

--- src/components/wheel.js
+++ src/components/wheel.js
@@ -7,7 +7,7 @@ const Wheel = props => {
     return (
         <div className="wheel-container">
             {   props.characters.map((character, i) => {
-                    const rotation = {transform: `translate(-50%, -50%) rotate(${(360 / numSpokes) * i}deg)`};
+                    const rotation = {transform: `translate(-50%, -50%) rotate(${((360 / numSpokes) * i) + 45}deg)`};
                     return (
                         <div key={i} className="wheel-spoke-container" style={rotation}>
                             <h1>{character}</h1>
