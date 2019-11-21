4c3b063736c5d5311c0057181541b9c65085938f
Kirk Hilse <khilsedev@gmail.com>
2019-11-21 11:51:08 -0800

    Added spin wheel marker

--- src/components/wheel.js
+++ src/components/wheel.js
@@ -5,17 +5,20 @@ const Wheel = props => {
     const numSpokes = props.characters.length;
 
     return (
-        <div className="wheel-container">
-            {   props.characters.map((character, i) => {
-                    const rotation = {transform: `translate(-50%, -50%) rotate(${((360 / numSpokes) * i) + 45}deg)`};
-                    return (
-                        <div key={i} className="wheel-spoke-container" style={rotation}>
-                            <h1>{character}</h1>
-                        </div>
-                    )
-                })
-            }
-        </div>
+        <>
+            <div className="wheel-container">
+                {   props.characters.map((character, i) => {
+                        const rotation = {transform: `translate(-50%, -50%) rotate(${((360 / numSpokes) * i) + 45}deg)`};
+                        return (
+                            <div key={i} className="wheel-spoke-container" style={rotation}>
+                                <h1>{character}</h1>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="marker"></div>
+        </>
     )
 }
 
