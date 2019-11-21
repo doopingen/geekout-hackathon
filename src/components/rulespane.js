351e07d8b7d59974410a579c31e80e5a5282d45d
Kirk Hilse <khilsedev@gmail.com>
2019-11-21 15:54:36 -0800

    Added rules content

--- src/components/rulespane.js
+++ src/components/rulespane.js
@@ -5,7 +5,12 @@ const RulesPane = props => {
     return (
         <div className="settings">
             <h1>Rules</h1>
-            <p>Deez are da Rulz</p>
+            <h2>How to Play</h2>
+            <ol>
+                <li><strong>Form your teams.</strong><br />Decide on your number of teams on the Settings pane, then name your teams, and decide who is acting first.</li>
+                <li><strong>Spin the wheel!</strong><br />It will land on your character and your key word(s) will appear. Don't share it with your teammates!</li>
+                <li><strong>Start the timer.</strong><br />In character, act out or give your team clues without saying the key word(s). You win a point if your team guesses the key word(s) before the time runs out!</li>
+            </ol>
             <div className="pane-nav-prev" onClick={props.handleGameStatePrev}>
                 <p>Home</p>
             </div>
