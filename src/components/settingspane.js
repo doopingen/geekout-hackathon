7bd4e407b6fecc43e170269438484f93a662f350
Kirk Hilse <khilsedev@gmail.com>
2019-11-22 10:23:50 -0800

    Incremental settings pane UI work

--- src/components/settingspane.js
+++ src/components/settingspane.js
@@ -5,12 +5,20 @@ const SettingsPane = props => {
     return (
         <div className="settings">
             <h1>Settings</h1>
-            <p>Sound</p>
-            <div className="pane-nav-prev" onClick={props.handleGameStateRules}>
+            <div>
+            <p>&nbsp;</p>
+            <p id="settingsSoundEffects" className="settings-item settings-item-name" onClick={props.handleChange}>Sound Effects <span id="soundValue" className="settings-item-value">{props.soundEffects ? 'On' : 'Off'}<span id="soundCarat" className="carat">></span></span></p>
+            <p id="settingsLanguage" className="settings-item settings-item-name" onClick={props.handleChange}>Language <span id="languageValue" className="settings-item-value">{props.language}<span id="languageCarat" className="carat">></span></span></p>
+            <p id="settingsTeamsCount" className="settings-item settings-item-name" onClick={props.handleChange}>Teams <span id="teamsValue" className="settings-item-value">{props.teams}<span id="teamsCarat" className="carat">></span></span></p>
+            <p id="settingsTimeLimit" className="settings-item settings-item-name" onClick={props.handleChange}>Time Limit Per Round <span className="settings-item-value">60 sec</span></p>
+            <p id="settingsRounds" className="settings-item settings-item-name" onClick={props.handleChange}>Rounds <span className="settings-item-value">5</span></p>
+            </div>
+            <p></p>
+            <div id="navPrev" className="pane-nav-prev" onClick={props.handleChange}>
                 <p>Rules</p>
             </div>
  
-            <div className="pane-nav-next" onClick={props.handleGameStateTheme}>
+            <div id="navNext" className="pane-nav-next" onClick={props.handleChange}>
                 <p>Select Theme</p>
             </div>
         </div>
